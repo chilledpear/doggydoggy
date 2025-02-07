e64c33b1debdc5111012bfc4db53bf1406d2d843
marywgachathi <marywgachathi@gmail.com>
2025-02-06 19:08:15 -0600

    hey

--- frontend/script.js
+++ frontend/script.js
@@ -9,11 +9,11 @@ document.getElementById("send-btn").addEventListener("click", sendMessage);
 function sendMessage() {
   const userInput = document.getElementById("user-input").value;
   if (userInput.trim() !== "") {
-    displayMessage("HODLER", userInput);
+    displayMessage("You", userInput);
     document.getElementById("user-input").value = "";
 
     fetchChatGPTResponse(userInput).then((response) => {
-      displayMessage("THE STRATEGIST", response);
+      displayMessage("Kate", response);
     });
   }
 }
